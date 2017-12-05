63a0522ea7acb1e63fc249b046119350e53b751a
Mira <szewczyk.mira@gmail.com>
2017-12-05 17:29:04 +0100

    Both parts from today

--- Day5/cpu.rb
+++ Day5/cpu.rb
@@ -1,4 +1,4 @@
-def main(offset_list=nil)
+def part1(offset_list=nil)
   unless offset_list
     offset_list = File.open('input.txt').map {|step| step.to_i}
   end
@@ -13,9 +13,21 @@ def main(offset_list=nil)
   counter_step
 end
 
-def test
-  raise unless main([0,3, 0, 1, -3]) == 5
+def part2
+  offset_list = File.open('input.txt').map {|step| step.to_i}
+  current_elem = 0
+  counter_step = 0
+  while current_elem < offset_list.length do
+    jump = offset_list[current_elem]
+    offset_list[current_elem] += (jump < 3 ? 1 : -1)
+    counter_step += 1
+    current_elem += jump
+  end
+  counter_step
 end
 
-test
-puts main
+def test
+  raise unless part1([0,3, 0, 1, -3]) == 5
+end
+
+puts part2
