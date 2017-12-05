a6b4a6125278d27f9c6ef0bf3014bcd2e5898317
Mira <szewczyk.mira@gmail.com>
2017-12-05 17:24:12 +0100

    Count steps in cpu

---
+++ Day5/cpu.rb
@@ -0,0 +1,21 @@
+def main(offset_list=nil)
+  unless offset_list
+    offset_list = File.open('input.txt').map {|step| step.to_i}
+  end
+  current_elem = 0
+  counter_step = 0
+  while current_elem < offset_list.length do
+    jump = offset_list[current_elem]
+    offset_list[current_elem] += 1
+    counter_step += 1
+    current_elem += jump
+  end
+  counter_step
+end
+
+def test
+  raise unless main([0,3, 0, 1, -3]) == 5
+end
+
+test
+puts main
