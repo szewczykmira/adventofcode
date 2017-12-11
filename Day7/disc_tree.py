49fad78386628f38eda5416b1d1e1649affa043e
Mira <szewczyk.mira@gmail.com>
2017-12-11 21:51:04 +0100

    Really awfull answear for 7/2

--- Day7/disc_tree.py
+++ Day7/disc_tree.py
@@ -1,6 +1,8 @@
 from collections import namedtuple, Counter
 
 Program = namedtuple('Program', ['weight', 'sons'])
+Node = namedtuple('Node', ['name', 'sum', 'weight', 'sons', 'equal'])
+Leaf = namedtuple('Leaf', ['name', 'sum', 'equal'])
 
 class MemoryTree:
     TOWER = {}
@@ -37,15 +39,30 @@ class MemoryTree:
         return list(set(self.TOWER.keys()) - self.sons)[0]
 
     def balance(self, elem):
-        elem = self.TOWER[elem]
-        sons = sum(self.balance(son) for son in elem.sons)
-        result = elem.weight + sons
-        return result
+        node = self.TOWER[elem]
+        if node.sons:
+            sons = [self.balance(son) for son in node.sons]
+            sons_weights = [son.sum for son in sons]
+            are_equal = len(set(sons_weights)) == 1
+            return Node(
+                elem, node.weight + sum(sons_weights), node.weight, sons, are_equal)
+        return Leaf(elem, node.weight, True)
 
-    def find_balance(self, node):
-        node_elem = self.TOWER[node]
-        sons = [self.balance(son) for son in node_elem.sons]
+    def is_balanced(self, balanced):
+        sums = [son.sum for son in balanced.sons]
+        if set(sums) == 1:
+            return balanced
+        count = Counter(sums)
+        count.subtract()
+        different = [x[0] for x in dict(count).items() if x[1] == 1][0]
+        different = [son for son in balanced.sons if son.sum == different][0]
+        diff_sons_sums = [son.sum for son in different.sons]
+        if len(set(diff_sons_sums)) == 1:
+            return [(son.weight, son.sum) for son in balanced.sons]
+        return self.is_balanced(different)
 
 
 tree = MemoryTree()
-print(tree.find_father())
+father = tree.find_father()
+balanced = tree.balance(father)
+print(tree.is_balanced(balanced))
