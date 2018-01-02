00259adeb88a430417af88cd3d71811710082296
Mira <szewczyk.mira@gmail.com>
2018-01-02 21:23:40 +0100

    Finish day 14

--- Day14/defragmentation.py
+++ Day14/defragmentation.py
@@ -1,4 +1,6 @@
+from collections import namedtuple
 from functools import reduce
+
 
 def parse_input(sequence):
     sequence = [ord(d) for d in sequence]
@@ -57,4 +59,74 @@ def ones(input):
 def find_ones(data):
     return sum(ones(elem) for elem in hashes_to_bin(data))
 
-print(find_ones('hxtvlmkl'))
+
+class Point:
+    def __init__(self, row, column, parent):
+        self.row = row
+        self.column = column
+        self.parent = parent
+
+    def is_it(self, row, column):
+        return (row == self.row) and (column == self.column)
+
+    def __str__(self):
+        return f"({self.row}, {self.column}): {self.parent}"
+
+    def __repr__(self):
+        return str(self)
+
+    def __eq__(self, other):
+        return self.row == other.row and self.column == other.column
+
+    def is_neighbour(self, other):
+        if self.row == other.row and self.column == (other.column + 1):
+            return True
+        return self.column == other.column and self.row == (other.row + 1)
+
+
+class UnionFind:
+    def __init__(self, value):
+        self.hashes = hashes_to_bin(value)
+        self.data = self.prepare()
+
+    def prepare(self):
+        iterator = 0
+        data = []
+        for row in range(0, 128):
+            for column in range(0, 128):
+                if self.hashes[row][column] == '1':
+                    data.append(Point(column, row, iterator))
+                    iterator += 1
+        return data
+
+    def union(self, groups):
+        if len(groups) < 2:
+            return
+        main_group = groups[0]
+        for elem in self.data:
+            if elem.parent in groups[1:]:
+                elem.parent = main_group
+
+    def walk(self):
+        for position in range(0, len(self.data)):
+            elem = self.data[position]
+            groups = [elem.parent]
+            for value in range(0, position):
+                other = self.data[value]
+                if elem.is_neighbour(other):
+                    groups.append(other.parent)
+            self.union(groups)
+
+    def __str__(self):
+        return str(self.data)
+
+    def how_many(self):
+        data = set()
+        for item in self.data:
+            data.add(item.parent)
+        return len(data)
+
+
+uf = UnionFind('hxtvlmkl')
+uf.walk()
+print(uf.how_many())
