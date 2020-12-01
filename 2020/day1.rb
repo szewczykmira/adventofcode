5c94f1f7d3722d7c6890f427cf75f886970bccdc
Mira <szewczyk.mira@gmail.com>
2020-12-01 22:12:47 +0100

    prettier code

--- 2020/day1.rb
+++ 2020/day1.rb
@@ -2,41 +2,26 @@ require 'set'
 
 EXPECTED_VALUE = 2020
 
-
 def one
     read_values = Set[]
-    multipla = 1
-
-    File.read("input_1_1.txt").each_line do |value|
-        ivalue = value.to_i
-        negative = EXPECTED_VALUE - ivalue
-        if read_values.include? negative
-            multipla = negative * ivalue
-        end
-        read_values.add ivalue
+    File.read("input_1_1.txt").lines.lazy.map(&:to_i).each do |value|
+        negative = EXPECTED_VALUE - value
+        return negative * value if read_values.include? negative
+        read_values << value
     end
-    multipla
 end
 
 
 def two
     read_values = Set[]
-    recieved_sums = Hash[]
-    multipla = 1
-    File.read("input_1_2.txt").each_line do |value|
-        ivalue = value.to_i
-        negative = EXPECTED_VALUE - ivalue
-        if recieved_sums.has_key? negative
-            i, j = recieved_sums[negative]
-            multipla = ivalue * i * j
-            break
-        end
-        recieved_sums.merge!(read_values.map do |v|
-            [v+ivalue, [v, ivalue]]
-        end.to_h)
-        read_values << ivalue
+    received_sums = Hash[]
+    File.read("input_1_2.txt").lines.lazy.map(&:to_i).each do |value|
+        negative = EXPECTED_VALUE - value
+        return [value, received_sums[negative]].flatten.reduce(:*) if received_sums.has_key? negative
+        received_sums.merge! (read_values.map { |v| [v+value, [v, value]]}.to_h)
+        read_values << value
     end
-    multipla
 end
 
+puts one
 puts two
