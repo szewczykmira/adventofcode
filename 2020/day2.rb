3562685824cf2144e5ce461e427ab4f04094c7ed
Mira <szewczyk.mira@gmail.com>
2020-12-04 21:59:13 +0100

    They say that this is a good way to use ruby...

--- 2020/day2.rb
+++ 2020/day2.rb
@@ -1,28 +1,25 @@
 def is_valid_1(line)
   *vals, letter, password = line.match(/(\d+)-(\d+) (\w{1}): (\w+)/).captures
   min, max = vals.map(&:to_i)
-  count = password.count letter
-  min <= count and count <= max
+  (min..max).include? password.count(letter)
 end
 
 def is_valid_2(line)
   *vals, letter, password = line.match(/(\d+)-(\d+) (\w{1}): (\w+)/).captures
   first, sec = vals.map { |v| v.to_i - 1 }
-  at_first = (password[first] == letter and not password[sec] == letter)
-  at_second = (not password[first] == letter and password[sec] == letter)
-  at_first or at_second
+  (password[first] == letter) ^ (password[sec] == letter)
 end
 
-def validator(filename, func)
-  (File.read(filename).lines.map { |line| method(func).call(line) }).select! { |el| el }.length
+def validator(filename, &func)
+  File.read(filename).lines.select(&func).length
 end
 
 def one
-  validator("input_2_1.txt", :is_valid_1)
+  validator("input_2_1.txt", &method(:is_valid_1))
 end
 
 def two
-  validator("input_2_2.txt", :is_valid_2)
+  validator("input_2_2.txt", &method(:is_valid_2))
 end
 
 puts one
--- 2020/day3.rb
+++ 2020/day3.rb
@@ -1,39 +1,19 @@
-OPEN_SQUARE = "."
 TREE = "#"
 
-def one_under(filename, slope)
-  current_column = 0
-  how_many_trees = 0
-  File.read(filename).lines.lazy.each do |line|
-    how_many_trees += 1 if line[current_column % (line.length - 1)] == TREE
-    current_column += slope
-  end
-  how_many_trees
-end
-
-def get_slopes(filename, right, under)
-  lines = File.read(filename).lines
-  current_row = 0
-  current_column = 0
-  how_many_trees = 0
-  while current_row < lines.length do
-    line = lines[current_row]
-    l_length = line.length
-    how_many_trees += 1 if line[current_column % (l_length - 1)] == TREE
-    current_row += under
-    current_column += right
-  end
-  how_many_trees
+def many_under(filename, slope, many)
+  File.read(filename).lines.select.with_index do |line, index|
+    line[(index / many) * slope % (line.length - 1)] == TREE and (index % many == 0)
+  end.count
 end
 
 def one
-  one_under("input_3_1.txt", 3)
+  many_under("input_3_1.txt", 3, 1)
 end
 
 def two
   filename = "input_3_2.txt"
   walks = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
-  walks.map { |right, under| get_slopes(filename, right, under) }.reduce(1, :*)
+  walks.map { |right, under| many_under(filename, right, under) }.reduce(:*)
 end
 
 puts one
