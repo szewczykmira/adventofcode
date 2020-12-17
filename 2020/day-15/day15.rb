30fa0947121ee4057632f0ad6359e90e69bf678a
Mira <szewczyk.mira@gmail.com>
2020-12-17 16:06:21 +0100

    Finish day 15th

--- 2020/day-15/day15.rb
+++ 2020/day-15/day15.rb
@@ -9,11 +9,11 @@ INPUT_ORIG = "5,2,8,16,18,0,1"
 
 INPUT = INPUT_ORIG
 
-def one
+def calculate(times)
   values = INPUT.split(",")[0..-2].each_with_index.map { |v, index| [v.to_i, index + 1] }.to_h
   prev_number = INPUT.split(",")[-1].to_i
   counter = values.count + 2
-  while counter < 2021
+  while counter < times + 1
     number = (values.key? prev_number) ? ((counter - 1) - values[prev_number]) : 0
     values[prev_number] = counter - 1
     prev_number = number
@@ -22,7 +22,13 @@ def one
   number
 end
 
+def one
+  calculate(2020)
+end
+
 def two
+  calculate(30000000)
 end
 
 p one
+p two
