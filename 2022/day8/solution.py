52e5dcd6155f78f0419a25a4a1daeb0628a29e12
Mira <6984271+szewczykmira@users.noreply.github.com>
2022-12-08 18:04:57 +0100

    Ugly part 2

--- 2022/day8/solution.py
+++ 2022/day8/solution.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 INPUT_FILE = "input.txt"
 
 with open(INPUT_FILE) as trees:
@@ -5,16 +7,43 @@ with open(INPUT_FILE) as trees:
 
 visible = len(trees[0]) * 2 + len(trees) * 2 - 4
 
+scenic_score = 0
+
 for i in range(1, len(trees) - 1):
     for j in range(1, len(trees) - 1):
         tree = trees[i][j]
         print(f"({i}, {j}): {tree}")
-        max_left = max(trees[i][:j])
-        max_right = max(trees[i][j + 1:])
-        max_up = max([trees[x][j] for x in range(0, i)])
-        max_down = max([trees[x][j] for x in range(i + 1, len(trees))])
+        # left = trees[i][:j][::-1]
+        # right = trees[i][j + 1:]
+        up = [trees[x][j] for x in range(0, i)][::-1]
+        down = [trees[x][j] for x in range(i + 1, len(trees))]
+        scores = []
+        for ind, l in enumerate(trees[i][:j][::-1], start=1):
+            if l >= tree:
+                scores.append(ind)
+                break
+        else:
+            scores.append(len(trees[i][:j]))
+        for ind, r in enumerate(trees[i][j + 1:], start=1):
+            if r >= tree:
+                scores.append(ind)
+                break
+        else:
+            scores.append(len(trees[i][j + 1:]))
+        for ind, u in enumerate(up, start=1):
+            if u >= tree:
+                scores.append(ind)
+                break
+        else:
+            scores.append(len(up))
 
-        if any([x < tree for x in [max_up, max_right, max_left, max_down]]):
-            visible += 1
+        for ind, u in enumerate(down, start=1):
+            if u >= tree:
+                scores.append(ind)
+                break
+        else:
+            scores.append(len(down))
 
-print(visible)
+        scenic_score = max(scenic_score, reduce(lambda x, y: x * y, scores))
+
+print(scenic_score)
